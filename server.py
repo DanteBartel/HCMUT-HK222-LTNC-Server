4ddf9cd6208948764fb2e0606a43a6910e87045c
DanteBartel <dantedbartel@gmail.com>
2023-04-28 18:04:16 +0700

    first commit

---
+++ server.py
@@ -0,0 +1,136 @@
+import asyncio
+import websockets
+import json
+
+# SERVERIP = '127.0.0.1'
+SERVERIP = '192.168.1.33'
+SERVERPORT = '8000'
+SEND_INTERVAL = 10
+
+async def sensor():
+    while True:
+        print("Running task1")
+        await asyncio.sleep(5)
+
+async def handle_websocket(websocket, path):
+    # Connection opened
+    client_ip, client_port = websocket.remote_address
+    print(f"New connection from {client_ip}:{client_port}")
+    try:
+        while True:
+            # Received message
+            message = await websocket.recv()
+            # Try to read the json file
+            try:
+                jsonObj = json.loads(message)
+                # Process JSON object here
+                if jsonObj['Type'] == 'Hello': 
+                    sendMessage = replyHello(jsonObj)
+                    await websocket.send(json.dumps(sendMessage))
+                elif jsonObj['Type'] == 'RequestUpdateSensor':
+                    sendMessage = replyRequestUpdateSensor(jsonObj)
+                    await websocket.send(json.dumps(sendMessage))
+                elif jsonObj['Type'] == 'RequestDeviceControl':
+                    sendMessage = replyRequestDeviceControl(jsonObj)
+                    await websocket.send(json.dumps(sendMessage))
+                elif jsonObj['Type'] == 'RequestDeviceTimerSchedule':
+                    sendMessage = replyRequestDeviceTimerSchedule(jsonObj)
+                    await websocket.send(json.dumps(sendMessage))
+                elif jsonObj['Type'] == 'RequestDeviceTimerBook':
+                    handleRequestDeviceTimerBook(jsonObj)
+                elif jsonObj['Type'] == 'RequestDeviceTimerDelete':
+                    handleRequestDeviceTimerDelete(jsonObj)
+            except json.JSONDecodeError:
+                # Invalid JSON received
+                continue
+            # Send sensor data every few second
+            while True:
+                await asyncio.sleep(SEND_INTERVAL)
+                with open('sensor.json', 'r+') as file:
+                    data = json.load(file)
+                    data['Motion'] = data['Motion'] * (-1)                
+                with open('sensor.json', 'w') as file:
+                    json.dump(data, file, indent=1)
+                await websocket.send(json.dumps(data))
+    except websockets.exceptions.ConnectionClosedOK:
+        # Connection closed
+        print(f"Connection from {client_ip}:{client_port} closed")
+        pass
+
+# ---------------- Function to handle messages
+def replyHello(jsonObj):
+    return {
+        'Type': 'ReplyHello',
+        'Message': jsonObj['Value']
+    }
+
+def replyRequestUpdateSensor(jsonObj):
+    with open('sensor.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+def replyRequestDeviceControl(jsonObj):
+    # Read the json file
+    deviceType = jsonObj['Device']
+    deviceValue = jsonObj['Value']
+    # Write a new JSONFile
+    with open('device.json', 'r') as file:
+        data = json.load(file)
+    if deviceType == 'Fan':
+        data['Fan'] = deviceValue
+    elif deviceType == 'Light':
+        data['Light'] = deviceValue
+    with open('device.json', 'w') as file:
+        json.dump(data, file)
+    # Print to terminal
+    print("Control " + deviceType + ": " + str(deviceValue))
+
+def replyRequestDeviceTimerSchedule(jsonObj):
+    # return a dict object
+    data = {
+        "Type": "DeviceTimerSchedule",
+        "Data": []
+    } 
+    with open('deviceSchedule.json', 'r') as file:
+        list = json.load(file)['Data']
+        data['Data'] += list
+    return data
+
+def handleRequestDeviceTimerBook(jsonObj):
+    # Read the deviceSchedule file
+    with open('deviceSchedule.json', 'r') as file:
+        data = json.load(file)
+    # Read the incomming jsonObj
+    newDict = {
+        'Device': jsonObj['Device'],
+        'Value': jsonObj['Value'],
+        'TimeStart': jsonObj['TimeStart']
+    }
+    # Write new file
+    data['Data'] += [newDict]
+    with open('deviceSchedule.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+def handleRequestDeviceTimerDelete(jsonObj):
+    # Read the deviceSchedule file
+    with open('deviceSchedule.json', 'r') as file:
+        data = json.load(file)
+    # Read the incomming jsonObj
+    position = jsonObj['Position']
+    # Write new file
+    print(position)
+    print(type(data))
+    data['Data'].pop(position)
+    with open('deviceSchedule.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+# ----------------------------------------------------------------
+async def main():
+    async with websockets.serve(handle_websocket, SERVERIP, SERVERPORT):
+        # Define task for sensor
+        # task1 = asyncio.create_task(sensor())
+        # await asyncio.gather(task1)
+        # Websocket Wait forever
+        await asyncio.Future()  
+
+asyncio.run(main())
